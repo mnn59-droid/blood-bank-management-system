a2d91608c4bf87f359daa745fb787c499e326163
Marvin nyakina <mnn59@scarletmail.rutgers.edu>
2026-09-23 09:25:02 -0400

    Add database configuration to config.local.php

---
+++ config.local.php
@@ -0,0 +1,6 @@
+<?php
+$DB_HOST     = 'sqlXXX.infinityfree.com';
+$DB_USER     = 'if0_42991277';
+$DB_PASSWORD = 'your-vPanel-password';
+$DB_NAME     = 'if0_42991277_bloodbank';
+$DB_PORT     = 3306;
